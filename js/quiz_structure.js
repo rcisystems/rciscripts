0e2131d4bac78e977e5837e3fd267645c09fdf25
Troy Powell <troypowell28@gmail.com>
2025-05-01 18:55:00 -0800

    adding additional text to results and adjusting formatting

--- js/quiz_structure.js
+++ js/quiz_structure.js
@@ -114,9 +114,11 @@ function calculateScore() {
                      "Portfolio Illusion – You’re Not Diversified, Just Overexposed.";
     
     document.getElementById("result").innerHTML = `
-        <h2>Your Diversification Results</h2>
-        <h3>Overall Score: ${totalScore} / ${maxTotalScore} (${totalPercentage.toFixed(1)}%)</h3>
-        <p style="margin-bottom: 20px;">${resultText}</p>
+        <h3>Your Diversification Results</h3>
+        <h2>Overall Score: ${totalScore} / ${maxTotalScore} (${totalPercentage.toFixed(1)}%)</h2>
+        <h4 style="margin-bottom: 20px;">${resultText}</h4>
+        <p>We’ll email your full report including your scores, insights, and personalized recommendations. </p>
+        <p style="margin-bottom: 20px;"> Just fill out the form below to get it delivered to your inbox.</p>
         <!-- Section scores hidden from display -->
     `;
     
